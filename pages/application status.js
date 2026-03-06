c058c3ea01a48452da049bc12160f9d6d4001d16
shrutiporje7 <shrutiporje7@gmail.com>
2026-03-06 00:04:41 -0800

    improved apllication status page

---
+++ pages/application status.js
@@ -0,0 +1,51 @@
+function checkStatus(){
+
+let id=document.getElementById("appId").value;
+let result=document.getElementById("result");
+
+if(id=="UJP101"){
+result.innerHTML="📩 Application Received";
+}
+
+else if(id=="UJP102"){
+result.innerHTML="🕒 Application Under Review";
+}
+
+else if(id=="UJP103"){
+result.innerHTML="📅 Interview Scheduled";
+}
+
+else if(id=="UJP104"){
+result.innerHTML="🎉 Congratulations! You are Selected";
+}
+
+else{
+result.innerHTML="❌ Application ID not found";
+}
+
+}function checkStatus(){
+
+let id=document.getElementById("appId").value;
+let result=document.getElementById("result");
+
+if(id=="UJP101"){
+result.innerHTML="📩 Application Received";
+}
+
+else if(id=="UJP102"){
+result.innerHTML="🕒 Application Under Review";
+}
+
+else if(id=="UJP103"){
+result.innerHTML="📅 Interview Scheduled";
+}
+
+else if(id=="UJP104"){
+result.innerHTML="🎉 Congratulations! You are Selected";
+}
+
+else{
+result.innerHTML="❌ Application ID not found";
+}
+
+}
